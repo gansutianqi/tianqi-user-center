0e2dd9b67c67530cd175ffc2e6e35cbbb7c2017c
buuug7 <youpp@126.com>
2017-12-11 13:56:59 +0800

    fix

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -1,91 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name') }} - Authorization</title>
+	<title>{{ config('app.name') }} - Authorization</title>
 
-    <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+	<!-- Styles -->
+	<link href="/css/app.css" rel="stylesheet">
 
-    <style>
-        .passport-authorize .container {
-            margin-top: 30px;
-        }
+	<style>
+		.passport-authorize .container {
+			margin-top: 30px;
+		}
 
-        .passport-authorize .scopes {
-            margin-top: 20px;
-        }
+		.passport-authorize .scopes {
+			margin-top: 20px;
+		}
 
-        .passport-authorize .buttons {
-            margin-top: 25px;
-            text-align: center;
-        }
+		.passport-authorize .buttons {
+			margin-top: 25px;
+			text-align: center;
+		}
 
-        .passport-authorize .btn {
-            width: 125px;
-        }
+		.passport-authorize .btn {
+			width: 125px;
+		}
 
-        .passport-authorize .btn-approve {
-            margin-right: 15px;
-        }
+		.passport-authorize .btn-approve {
+			margin-right: 15px;
+		}
 
-        .passport-authorize form {
-            display: inline;
-        }
-    </style>
+		.passport-authorize form {
+			display: inline;
+		}
+	</style>
 </head>
 <body class="passport-authorize">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        授权请求
-                    </div>
-                    <div class="panel-body">
-                        <!-- Introduction -->
-                        <p><strong>{{ $client->name }}</strong> 请求允许进入你的帐户。</p>
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					授权请求
+				</div>
+				<div class="panel-body">
+					<!-- Introduction -->
+					<p><strong>{{ $client->name }}</strong> 请求允许进入你的帐户。</p>
 
-                        <!-- Scope List -->
-                        @if (count($scopes) > 0)
-                            <div class="scopes">
-                                    <p><strong>这个应用程序将能够：</strong></p>
+					<!-- Scope List -->
+					@if (count($scopes) > 0)
+						<div class="scopes">
+							<p><strong>这个应用程序将能够：</strong></p>
 
-                                    <ul>
-                                        @foreach ($scopes as $scope)
-                                            <li>{{ $scope->description }}</li>
-                                        @endforeach
-                                    </ul>
-                            </div>
-                        @endif
+							<ul>
+								@foreach ($scopes as $scope)
+									<li>{{ $scope->description }}</li>
+								@endforeach
+							</ul>
+						</div>
+					@endif
 
-                        <div class="buttons">
-                            <!-- Authorize Button -->
-                            <form method="post" action="/oauth/authorize">
-                                {{ csrf_field() }}
+					<div class="buttons">
+						<!-- Authorize Button -->
+						<form method="post" action="/oauth/authorize">
+							{{ csrf_field() }}
 
-                                <input type="hidden" name="state" value="{{ $request->state }}">
-                                <input type="hidden" name="client_id" value="{{ $client->id }}">
-                                <button type="submit" class="btn btn-success btn-approve">授权</button>
-                            </form>
+							<input type="hidden" name="state" value="{{ $request->state }}">
+							<input type="hidden" name="client_id" value="{{ $client->id }}">
+							<button type="submit" class="btn btn-success btn-approve">授权</button>
+						</form>
 
-                            <!-- Cancel Button -->
-                            <form method="post" action="/oauth/authorize">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
+						<!-- Cancel Button -->
+						<form method="post" action="/oauth/authorize">
+							{{ csrf_field() }}
+							{{ method_field('DELETE') }}
 
-                                <input type="hidden" name="state" value="{{ $request->state }}">
-                                <input type="hidden" name="client_id" value="{{ $client->id }}">
-                                <button class="btn btn-danger">取消</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+							{{ dd($request) }}
+							<input type="hidden" name="state" value="{{ $request->state }}">
+							<input type="hidden" name="client_id" value="{{ $client->id }}">
+							<button class="btn btn-danger">取消</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 </body>
 </html>
